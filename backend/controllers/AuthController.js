1b1737bc267c7dbf34bc359c2e48315af253f264
yeshwanth-poloju <yeshwanthpoloju@gmail.com>
2024-10-16 17:00:42 +0530

    added token

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,17 +1,21 @@
 // /backend/controllers/AuthController.js
 const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
 
 dotenv.config();
 
 const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+const JWT_SECRET = process.env.JWT_SECRET; // Add a secret key in your .env file
 
 exports.login = (req, res) => {
   const { email, password } = req.body;
 
   // Check if the credentials match the hardcoded values
   if (email === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
-    // You can implement token generation or session management here
+    // Generate JWT Token
+    const token = jwt.sign({ username: ADMIN_USERNAME }, JWT_SECRET, { expiresIn: '1h' });
+   
     res.status(200).json({ message: 'Login successful' });
   } else {
     res.status(401).json({ message: 'Invalid username or password' });
--- backend/routes/tripRoutes.js
+++ backend/routes/tripRoutes.js
@@ -2,10 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const { addTrip, getTrips, editTrip, deleteTrip } = require('../controllers/tripController');
+const authenticateToken = require('../middleware/authMiddleware');
 
-router.post('/trips', addTrip);
-router.get('/trips', getTrips);
-router.put('/trips/:id', editTrip);  // Edit Trip Route
-router.delete('/trips/:id', deleteTrip);  // Delete Trip Route
+router.post('/trips',authenticateToken, addTrip);
+router.get('/trips',authenticateToken, getTrips);
+router.put('/trips/:id',authenticateToken, editTrip);  // Edit Trip Route
+router.delete('/trips/:id',authenticateToken, deleteTrip);  // Delete Trip Route
 
 module.exports = router;
